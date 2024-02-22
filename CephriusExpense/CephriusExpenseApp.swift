b2f77f7fe5c9d0d19fea66dbac5fbdaf07991f0f
Chiedozie Ehileme <ehilemece@hc.edu>
2024-02-22 11:32:41 -0600

    Initial Commit

---
+++ CephriusExpense/CephriusExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CephriusExpenseApp.swift
+//  CephriusExpense
+//
+//  Created by Chiedozie Ehileme on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CephriusExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
